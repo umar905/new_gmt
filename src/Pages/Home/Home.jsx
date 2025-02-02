102ab2e8d5b3348848e9dde84daf1bba94ed9c02
umar905 <u.b.h.hajji@gmail.com>
2025-02-02 13:34:31 +0500

    Update Home.jsx

--- src/Pages/Home/Home.jsx
+++ src/Pages/Home/Home.jsx
@@ -17,7 +17,7 @@ const Home = () => {
         <Categories/>
         <SecondSlider/>
         <CategoriesSlider/>
-        <BrendsSlider/>
+        {/* <BrendsSlider/> */}
         <AccordionSection/>
         <Mapsection/>
         <SendMsg/>
